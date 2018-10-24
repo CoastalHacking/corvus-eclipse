1dcb2269a293b18569e07198a7695560e3c1b19e
Jon Passki <jon@passki.us>
2018-10-24 16:10:41 -0700

    Minor updates to launcher

--- bundles/us.coastalhacking.corvus.eclipse.launcher.provider/src/us/coastalhacking/corvus/eclipse/launcher/provider/CorvusLauncherBase.java
+++ bundles/us.coastalhacking.corvus.eclipse.launcher.provider/src/us/coastalhacking/corvus/eclipse/launcher/provider/CorvusLauncherBase.java
@@ -1,29 +1,18 @@
 package us.coastalhacking.corvus.eclipse.launcher.provider;
 
-import java.text.MessageFormat;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Map;
 
-import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.operations.IOperationHistory;
 import org.eclipse.core.commands.operations.IUndoContext;
-import org.eclipse.core.commands.operations.IUndoableOperation;
-import org.eclipse.core.commands.operations.ObjectUndoContext;
-import org.eclipse.core.commands.operations.OperationHistoryFactory;
-import org.eclipse.core.resources.IResourceChangeListener;
-import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
-import org.eclipse.emf.workspace.AbstractEMFOperation;
-import org.eclipse.emf.workspace.WorkspaceEditingDomainFactory;
 
 import us.coastalhacking.corvus.eclipse.transaction.EclipseTransactionApi;
 import us.coastalhacking.corvus.eclipse.transaction.ResourceInitializer;
@@ -45,32 +34,29 @@ public abstract class CorvusLauncherBase {
 		// Initialize resources
 		for (ResourceInitializer ri : getInitializers()) {
 			final String pathValue = (String) props.get(ri.getPathKey());
-			final IUndoableOperation operation = new AbstractEMFOperation(getDomain(),
-					MessageFormat.format("Initalizing Corvus Launcher URI {0}", ri.getUriKey())) {
-				@Override
-				protected IStatus doExecute(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
-					try {
-						URI key = URI.createURI(ri.getUriKey());
-						URI value = URI.createFileURI(pathValue);
-						getResourceSet().getURIConverter().getURIMap().put(key, value);
+			URI key = URI.createURI(ri.getUriKey());
+			URI value = URI.createFileURI(pathValue);
 
-						Resource resource;
-						try {
-							resource = getResourceSet().getResource(key, true);
-						} catch (Exception e) {
-							resource = getResourceSet().getResource(key, false);
-						}
-						resource.getContents().add(ri.getRoot());
-						// TODO: get XMI options and pass here via properties
-						resource.save(null);
-						return Status.OK_STATUS;
+			final Command command = new RecordingCommand(getDomain()) {
+				@Override
+				protected void doExecute() {
+					getResourceSet().getURIConverter().getURIMap().put(key, value);
+					Resource resource;
+					try {
+						resource = getResourceSet().getResource(key, true);
 					} catch (Exception e) {
-						throw new ExecutionException(e.getMessage(), e);
+						resource = getResourceSet().getResource(key, false);
+					}
+					resource.getContents().add(ri.getRoot());
+					// TODO: get XMI options and pass here via properties
+					try {
+						resource.save(null);
+					} catch (IOException e) {
+						throw new RuntimeException(e);
 					}
 				}
 			};
-			operation.addContext(getUndoContext());
-			getOperationHistory().execute(operation, new NullProgressMonitor(), null);
+			getDomain().getCommandStack().execute(command);
 		}
 
 		// TODO: Add adapters
@@ -92,35 +78,17 @@ public abstract class CorvusLauncherBase {
 		return resourceSet;
 	}
 
-	protected IOperationHistory getOperationHistory() {
-		if (operationHistory == null) {
-			operationHistory = OperationHistoryFactory.getOperationHistory();
-		}
-		return operationHistory;
-	}
-
 	protected TransactionalEditingDomain getDomain() {
 		if (domain == null) {
-			domain = getDomainFactory().createEditingDomain(getResourceSet(), getOperationHistory());
+			domain = getDomainFactory().createEditingDomain(getResourceSet());
 			getDomainRegistry().add(transactionId, domain);
 		}
 		return domain;
 	}
 
-	protected IUndoContext getUndoContext() {
-		if (undoContext == null) {
-			undoContext = new ObjectUndoContext(this);
-		}
-		return undoContext;
-	}
-
-	protected abstract WorkspaceEditingDomainFactory getDomainFactory();
+	protected abstract TransactionalEditingDomain.Factory getDomainFactory();
 
 	protected abstract TransactionalEditingDomain.Registry getDomainRegistry();
 
 	protected abstract Collection<ResourceInitializer> getInitializers();
-	
-//	protected abstract IWorkspace getWorkspace();
-//	
-//	protected abstract IResourceChangeListener getChangeListener(); 
 }
--- tests/us.coastalhacking.corvus.eclipse.resources.provider.tests/src/us/coastalhacking/corvus/eclipse/resources/provider/EclipseResourcesChangeListenerBaseTest.java
+++ tests/us.coastalhacking.corvus.eclipse.resources.provider.tests/src/us/coastalhacking/corvus/eclipse/resources/provider/EclipseResourcesChangeListenerBaseTest.java
@@ -1,9 +1,9 @@
 package us.coastalhacking.corvus.eclipse.resources.provider;
 
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.mock;
